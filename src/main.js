d01ab722a60a35a42437fc7cdee6eba5b0319189
gaozhanbooo <gaozhanbooo@163.com>
2022-07-12 13:11:00 +0800

    添加tailwindcss

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import './assets/tailwind.css'
 
 /**
  * If you don't want to use mock-server
